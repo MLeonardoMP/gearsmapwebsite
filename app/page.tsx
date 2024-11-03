1000b7c8616c515c862a4ed6264f0f7e2c2b2d63
Leonardo Mosquera <maleo_7@msn.com>
2024-11-02 21:28:38 -0500

    favicon and meta image

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,26 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "GearsMap",
   description: "Esta es la página web de GearsMap",
+  metadataBase: new URL("https://gearsmap.com"),
+  openGraph: {
+    title: "GearsMap",
+    description: "Esta es la página web de GearsMap",
+    url: "https://gearsmap.com",
+    images: [
+      {
+        url: "/images/logo/gearmaplogo.png",
+        width: 800,
+        height: 600,
+        alt: "Logo GearsMap",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "GearsMap",
+    description: "Esta es la página web de GearsMap",
+    images: "/images/logo/gearmaplogo.png",
+  },
   // other metadata
 };
 
